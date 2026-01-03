c36a6065e06fb5334f0b000a80913e3427221c81
MargusS <agusnicolas21@gmail.com>
2026-01-03 20:31:03 +0100

    Base project

---
+++ src/main/java/dev/margus/movie_expert_AI/MovieExpertAiApplication.java
@@ -0,0 +1,13 @@
+package dev.margus.movie_expert_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieExpertAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieExpertAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/margus/movie_expert_AI/MovieExpertAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.margus.movie_expert_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieExpertAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
